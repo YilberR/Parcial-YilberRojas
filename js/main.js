4069a070e5cff558fc9dc49ccdf4d47114491654
yilber <yilrojas@unbosque.edu.co>
2021-09-16 19:59:16 -0500

    web

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
 
-     let palabra =document.getElementById("palabra1");
-     let url='https://api.dictionaryapi.dev/api/v2/entries/en/'+palabra.value;
-     document.getElementById("botonBuscar").onclick=function(){
-     console.log(palabra.value);   
-     fetch(url)
-    .then(response => response.json())
-    .then(data => mostrarData(data))
-    .catch(error => console.log(error)) 
-     const mostrarData = (data) =>{
-     console.log(data);
-      };       
-     };
+document.getElementById("botonBuscar").onclick=function(){
+        let div=document.createElement("div");
+        let palabra =document.getElementById("palabra1").value;
+        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+palabra)
+       .then(response => response.json())
+       .then(data => {
+           data.forEach((mostrar)=>{
+           let significados = data.meanings;
+            significados.forEach(a){
+                let definicion=a.definitions;
+                let usos=a.partOfSpeech;
+                let p=document.createElement("p");
+                p.innerHTML=usos;
+                div.appendChild(p);
+                definicion.forEach((q){
+                let p=document.createElement("p");
+                p.innerHTML=q.definition
+                div.appendChild(p);
+                if(q.example!=undefined){
+                    let p=document.createElement("p");
+                    p.innerHTML=q.example
+                    div.appendChild(p);
+                }
+
+                })
+            
+            };
+
+                });
+           document.getElementById("resul").appendChild(div)
+
+
+                console.log(data);
+             
+        })    
+}
+      
